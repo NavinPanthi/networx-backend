883fff812ad083fdba769830abbcf07f3e8321f3
NavinPanthi <panthinabin341@gmail.com>
2026-06-14 07:10:11 +0100

    properties updated

---
+++ src/main/java/com/networx/networx/NetworxApplication.java
@@ -0,0 +1,13 @@
+package com.networx.networx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/networx/networx/NetworxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networx.networx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
